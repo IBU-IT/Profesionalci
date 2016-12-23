c898d5b26c921637ecbe871a4ad1568a3a0c18f7
Venan24 <venanosmic24@gmail.com>
2016-12-23 01:15:46 +0100

    Diazjn završen i LogOut

--- Pool/src/LogovanKorisnik.java
+++ Pool/src/LogovanKorisnik.java
@@ -1,6 +1,11 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class LogovanKorisnik {
@@ -36,8 +41,43 @@ public class LogovanKorisnik {
 	private void initialize() {
 		frmUserPanel = new JFrame();
 		frmUserPanel.setTitle("User Panel");
-		frmUserPanel.setBounds(100, 100, 450, 176);
+		frmUserPanel.setBounds(100, 100, 400, 206);
 		frmUserPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmUserPanel.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Prijavljen Kao:");
+		lblNewLabel.setBounds(10, 10, 85, 23);
+		frmUserPanel.getContentPane().add(lblNewLabel);
+		
+		JLabel lblIme = new JLabel("Ime:");
+		lblIme.setBounds(10, 37, 85, 23);
+		frmUserPanel.getContentPane().add(lblIme);
+		
+		JLabel lblPrezime = new JLabel("Prezime:");
+		lblPrezime.setBounds(10, 66, 85, 23);
+		frmUserPanel.getContentPane().add(lblPrezime);
+		
+		JButton btnGlasajNaAnketi = new JButton("Glasaj na Anketi");
+		btnGlasajNaAnketi.setBounds(232, 5, 137, 23);
+		frmUserPanel.getContentPane().add(btnGlasajNaAnketi);
+		
+		JButton btnAnketeNaKojim = new JButton("<html><body><center><p>Ankete na kojim<br>si glasao</p></center></body></html>");
+		btnAnketeNaKojim.setBounds(232, 69, 137, 53);
+		frmUserPanel.getContentPane().add(btnAnketeNaKojim);
+		
+		JButton btnPrijaviGreku = new JButton("Prijavi Gre\u0161ku");
+		btnPrijaviGreku.setBounds(232, 37, 137, 23);
+		frmUserPanel.getContentPane().add(btnPrijaviGreku);
+		
+		JButton btnodjaviSe = new JButton("<html><body><center><p>ODJAVI SE</p></center></body></html>");
+		btnodjaviSe.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		btnodjaviSe.setBounds(10, 133, 359, 28);
+		frmUserPanel.getContentPane().add(btnodjaviSe);
 	}
 
 }
