1d9ccad8f5948df74b8467ed8b7bcf217b3e6234
Venan24 <venanosmic24@gmail.com>
2016-12-25 23:28:25 +0100

    registracija izbacena slika

--- Pool/src/RegistracijaKorisnika.java
+++ Pool/src/RegistracijaKorisnika.java
@@ -28,10 +28,9 @@ import javax.swing.JRadioButton;
 
 public class RegistracijaKorisnika {
 
-	private JFrame frame;
+	private JFrame frmRegistrujSe;
 	/*private final Action action = new SwingAction();*/
 	JButton button;
-	JLabel lblAddPicture;
 	Image img;
 	//private Alert alert;
 	private JTextField textField;
@@ -48,7 +47,7 @@ public class RegistracijaKorisnika {
 			public void run() {
 				try {
 					RegistracijaKorisnika window = new RegistracijaKorisnika();
-					window.frame.setVisible(true);
+					window.frmRegistrujSe.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -67,122 +66,84 @@ public class RegistracijaKorisnika {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 322);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-	
-		
-		JButton btnNewButton = new JButton("Browse");
-		
-		
-		btnNewButton.addActionListener(new ActionListener() {
-			
-				public void actionPerformed(ActionEvent e) {
-					JFileChooser chooser = new JFileChooser();
-					FileNameExtensionFilter filter=new FileNameExtensionFilter("Images", "jpg","gif","png");
-					  chooser.addChoosableFileFilter(filter);
-					chooser.showOpenDialog(null);
-				    
-				 try{
-					 img=ImageIO.read(chooser.getSelectedFile());
-				 }catch(IOException e1){
-					 e1.printStackTrace();
-				}
-					ImageIcon icon = new ImageIcon(img); 
-				//lblAddPicture.setIcon(icon);
-					lblAddPicture.setIcon(ResizeImage(icon));
-			}
-				ImageIcon ResizeImage(ImageIcon i){
-					Image img= i.getImage();
-					Image newimg=img.getScaledInstance(lblAddPicture.getWidth(), lblAddPicture.getHeight(), Image.SCALE_SMOOTH);
-					ImageIcon image= new ImageIcon(newimg);
-					return image;}
-		});
-		
-		btnNewButton.setBounds(23, 160, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-		
-		
-		lblAddPicture= new JLabel();
-		lblAddPicture.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblAddPicture.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAddPicture.setText("ADD PICTURE");
-		lblAddPicture.setBounds(23, 11, 89, 138);
-		frame.getContentPane().add(lblAddPicture);
+		frmRegistrujSe = new JFrame();
+		frmRegistrujSe.setTitle("Registruj Se");
+		frmRegistrujSe.setBounds(100, 100, 256, 266);
+		frmRegistrujSe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRegistrujSe.getContentPane().setLayout(null);
 		
 		JLabel lblName = new JLabel("Name");
 		lblName.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblName.setBounds(200, 32, 46, 14);
-		frame.getContentPane().add(lblName);
+		lblName.setBounds(10, 13, 46, 14);
+		frmRegistrujSe.getContentPane().add(lblName);
 		
 		textField = new JTextField();
-		textField.setBounds(266, 30, 153, 20);
-		frame.getContentPane().add(textField);
+		textField.setBounds(76, 11, 153, 20);
+		frmRegistrujSe.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		
 			
 		JLabel lblSurname = new JLabel("Surname");
 		lblSurname.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblSurname.setBounds(200, 57, 51, 14);
-		frame.getContentPane().add(lblSurname);
+		lblSurname.setBounds(10, 38, 51, 14);
+		frmRegistrujSe.getContentPane().add(lblSurname);
 		
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(266, 55, 153, 20);
-		frame.getContentPane().add(textField_1);
+		textField_1.setBounds(76, 36, 153, 20);
+		frmRegistrujSe.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblAge = new JLabel("Age");
 		lblAge.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblAge.setBounds(200, 82, 46, 14);
-		frame.getContentPane().add(lblAge);
+		lblAge.setBounds(10, 63, 46, 14);
+		frmRegistrujSe.getContentPane().add(lblAge);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(266, 80, 51, 20);
-		frame.getContentPane().add(spinner);
+		spinner.setBounds(76, 61, 51, 20);
+		frmRegistrujSe.getContentPane().add(spinner);
 		
 		JLabel lblNewLabel = new JLabel("Username");
 		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblNewLabel.setBounds(200, 107, 67, 14);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(10, 88, 67, 14);
+		frmRegistrujSe.getContentPane().add(lblNewLabel);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(266, 105, 153, 20);
-		frame.getContentPane().add(textField_2);
+		textField_2.setBounds(76, 86, 153, 20);
+		frmRegistrujSe.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblPassword.setBounds(200, 132, 67, 14);
-		frame.getContentPane().add(lblPassword);
+		lblPassword.setBounds(10, 113, 67, 14);
+		frmRegistrujSe.getContentPane().add(lblPassword);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(266, 129, 153, 20);
-		frame.getContentPane().add(passwordField);
+		passwordField.setBounds(76, 110, 153, 20);
+		frmRegistrujSe.getContentPane().add(passwordField);
 		
 		JLabel lblGendre = new JLabel("Gender");
 		lblGendre.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblGendre.setBounds(200, 159, 67, 14);
-		frame.getContentPane().add(lblGendre);
+		lblGendre.setBounds(10, 140, 67, 14);
+		frmRegistrujSe.getContentPane().add(lblGendre);
 		
 		final JRadioButton Male = new JRadioButton("Male");
 		Male.setFont(new Font("Arial", Font.PLAIN, 11));
-		Male.setBounds(266, 160, 51, 23);
-		frame.getContentPane().add(Male);
+		Male.setBounds(76, 141, 51, 23);
+		frmRegistrujSe.getContentPane().add(Male);
 		
 		final JRadioButton Female = new JRadioButton("Female");
 		Female.setFont(new Font("Arial", Font.PLAIN, 11));
-		Female.setBounds(335, 160, 109, 23);
-		frame.getContentPane().add(Female);
+		Female.setBounds(157, 140, 109, 23);
+		frmRegistrujSe.getContentPane().add(Female);
 		
 		
 		
-		JButton Done = new JButton("Done");
+		JButton Done = new JButton("Registruj Se");
 		Done.setFont(new Font("Arial", Font.PLAIN, 13));
-		Done.setBounds(335, 224, 89, 36);
-		frame.getContentPane().add(Done);
+		Done.setBounds(10, 171, 219, 44);
+		frmRegistrujSe.getContentPane().add(Done);
 		Done.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(!Male.isSelected()&&!Female.isSelected()){
