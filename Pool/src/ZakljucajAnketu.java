7a143173edcc9aec9bd6d42fcdc6083a5a79bb2d
AtifKotoric <pa@daj.ba>
2016-12-25 23:56:45 +0100

    Zakljucaj anketu -> Dodavanje u GUI

--- Pool/src/ZakljucajAnketu.java
+++ Pool/src/ZakljucajAnketu.java
@@ -1,6 +1,11 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 
 public class ZakljucajAnketu {
@@ -35,8 +40,30 @@ public class ZakljucajAnketu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("Zakljucaj anketu");
+		frame.setBounds(100, 100, 512, 373);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblZakljucajAnketu = new JLabel("ZAKLJUCAJ ANKETU");
+		lblZakljucajAnketu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblZakljucajAnketu.setBounds(143, 37, 207, 45);
+		frame.getContentPane().add(lblZakljucajAnketu);
+		
+		JButton btnZakljucaj = new JButton("ZAKLJUCAJ ");
+		btnZakljucaj.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnZakljucaj.setBounds(87, 234, 153, 45);
+		frame.getContentPane().add(btnZakljucaj);
+		
+		JButton btnOdaberiDrugu = new JButton("ODABERI DRUGU");
+		btnOdaberiDrugu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnOdaberiDrugu.setBounds(250, 235, 185, 44);
+		frame.getContentPane().add(btnOdaberiDrugu);
+		
+		JTextArea txtrListaAnketa = new JTextArea();
+		txtrListaAnketa.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		txtrListaAnketa.setText("LISTA ANKETA");
+		txtrListaAnketa.setBounds(87, 85, 349, 133);
+		frame.getContentPane().add(txtrListaAnketa);
 	}
-
 }
