b815c31bd279704bb173990dd767a2ed1504af4e
AtifKotoric <pa@daj.ba>
2016-12-26 16:46:42 +0100

    Logaovan admin -> obrisan visak

--- Pool/src/LogovanAdmin.java
+++ Pool/src/LogovanAdmin.java
@@ -9,8 +9,7 @@ import javax.swing.JLabel;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+
 
 
 public class LogovanAdmin extends Login{
