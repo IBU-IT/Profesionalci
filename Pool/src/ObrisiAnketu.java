71ad19b7ce640203005d40a7eb3d7ebc67db2768
AtifKotoric <pa@daj.ba>
2016-12-26 15:26:05 +0100

    Brisanje ankete -> početak

---
+++ Pool/src/ObrisiAnketu.java
@@ -0,0 +1,56 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+
+public class ObrisiAnketu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ObrisiAnketu window = new ObrisiAnketu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ObrisiAnketu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Obrisi Anketu");
+		frame.setBounds(100, 100, 519, 398);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnObrii = new JButton("OBRI\u0160I ");
+		btnObrii.setBounds(144, 264, 213, 59);
+		frame.getContentPane().add(btnObrii);
+		
+		JLabel lblBrisanjeAnkete = new JLabel("BRISANJE ANKETE");
+		lblBrisanjeAnkete.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblBrisanjeAnkete.setBounds(160, 11, 197, 50);
+		frame.getContentPane().add(lblBrisanjeAnkete);
+	}
+
+}
