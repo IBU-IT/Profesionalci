286014dbbb454bbe045ff8e979801daf623e77d9
AtifKotoric <pa@daj.ba>
2016-12-28 22:28:09 +0100

    Zakljucaj anketu --> Povezano s bazon, Radi zakljucavanje, funkcionalno !

--- Pool/src/ZakljucajAnketu.java
+++ Pool/src/ZakljucajAnketu.java
@@ -3,13 +3,33 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.SystemColor;
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ZakljucajAnketu {
 
+	// Definisi JDBC driver name i URL baze
+		static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+		static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/SurveyDB?verifyServerCertificate=false&useSSL=false";
+
+		// Db podaci
+		static final String USER = "root";
+		static final String PASS = "123456";
+		
+		ArrayList<String> groupNames = new ArrayList<String>();
 	private JFrame frame;
 
 	/**
@@ -51,11 +71,7 @@ public class ZakljucajAnketu {
 		lblZakljucajAnketu.setBounds(143, 22, 207, 45);
 		frame.getContentPane().add(lblZakljucajAnketu);
 		
-		JButton btnZakljucaj = new JButton("ZAKLJUCAJ ");
-		btnZakljucaj.setBackground(SystemColor.activeCaption);
-		btnZakljucaj.setFont(new Font("Gadugi", Font.BOLD, 16));
-		btnZakljucaj.setBounds(50, 278, 200, 36);
-		frame.getContentPane().add(btnZakljucaj);
+		
 		
 		JButton btnOdaberiDrugu = new JButton("ODABERI DRUGU");
 		btnOdaberiDrugu.setBackground(SystemColor.activeCaption);
@@ -63,6 +79,93 @@ public class ZakljucajAnketu {
 		btnOdaberiDrugu.setBounds(260, 278, 200, 36);
 		frame.getContentPane().add(btnOdaberiDrugu);
 		
+		//TRY
+				Connection conn = null;
+				Statement stmt = null;
+				
+				
+				try {
+					// Registruj JDBC driver
+					Class.forName("com.mysql.jdbc.Driver");
+
+					// Zapocni konekciju conn
+					conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/SurveyDB?verifyServerCertificate=false&useSSL=false", "root", "123456");	
+					
+					stmt = conn.createStatement();
+					String sql;
+					sql = ("SELECT question_text FROM questions WHERE is_closed=0");
+					ResultSet rs = stmt.executeQuery(sql);
+				
+					while (rs.next()) { 
+					    String groupName = rs.getString("question_text"); 
+					    // add group names to the array list
+					    groupNames.add(groupName);
+					} 
+					
+					rs.close(); 
+					stmt.close();
+					conn.close();
+				} catch (SQLException se) {
+					// Errors JDBC
+					se.printStackTrace();
+				}
+				catch (Exception x) {
+					// Errors za Class.forName
+					x.printStackTrace();
+				}
+				//TRY
+				
+				final JComboBox comboBoxZakljucajAnketu = new JComboBox();
+				comboBoxZakljucajAnketu.setBounds(238, 64, 207, 20);
+				frame.getContentPane().add(comboBoxZakljucajAnketu);
+				DefaultComboBoxModel model = new DefaultComboBoxModel(groupNames.toArray());
+				comboBoxZakljucajAnketu.setModel(model);
+				
+				JButton btnZakljucaj = new JButton("ZAKLJUCAJ ");
+				btnZakljucaj.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						String zakkank = (String)comboBoxZakljucajAnketu.getSelectedItem();
+						// Spremi u varijable 
+						
+						Connection conn = null;
+						Statement stmt = null;
+						try {
+							// Registruj JDBC driver
+							Class.forName("com.mysql.jdbc.Driver");
+
+							// Zapocni konekciju conn
+							conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/SurveyDB?verifyServerCertificate=false&useSSL=false", "root", "123456");
+							stmt = conn.createStatement();
+							
+							int zakAnk = stmt.executeUpdate("UPDATE questions SET is_closed = 1 WHERE question_text = '"+zakkank+"'");
+							if(zakAnk>0){
+								JOptionPane.showMessageDialog(null, "Anketa uspjesno zakljucana");
+								UgasiGa();
+							}
+							
+	
+							stmt.close();
+							conn.close();
+						} catch (SQLException se) {
+							// Errors JDBC
+							se.printStackTrace();
+						}
+						catch (Exception x) {
+							// Errors za Class.forName
+							x.printStackTrace();
+						}
+					}
+				});
+				btnZakljucaj.setBackground(SystemColor.activeCaption);
+				btnZakljucaj.setFont(new Font("Gadugi", Font.BOLD, 16));
+				btnZakljucaj.setBounds(50, 278, 200, 36);
+				frame.getContentPane().add(btnZakljucaj);
 		
+		
+	}
+
+	public void UgasiGa() {
+		frame.dispose();
 	}
 }
